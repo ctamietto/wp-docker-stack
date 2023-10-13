c449e556f2d5b61cee6b55ef068f0aa727af0b30
c.tamietto <c.tamietto@pokerspa.it>
2023-10-13 11:55:18 +0200

    first commit

---
+++ move_wp_content.sh
@@ -0,0 +1,8 @@
+cd /var/lib/docker/volumes/wp-docker-stack_wordpress_root_data/_data
+rm wp-config.php
+rm wp-content
+cp -p ../../wp-docker-stack_wordpress_data/_data/wp-config.php .
+chown -R daemon:root wp-config.php
+rsync -a ../../wp-docker-stack_wordpress_data/_data/wp-content/ wp-content/
+chown -R daemon:root wp-content
+
